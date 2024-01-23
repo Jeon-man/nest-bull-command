7b26699b82755e4ee15f7460315448be82d4f2d3
Jeon-man <jhmjhm0106@naver.com>
2024-01-23 16:51:27 +0900

    FEAT: Add get failed job command & log form

--- src/command/subCommand/bull.jobs.command.ts
+++ src/command/subCommand/bull.jobs.command.ts
@@ -13,11 +13,39 @@ export class BullJobCommand extends CommandRunner {
     super();
   }
 
+  private formatText(input: string, maxLineLength: number): string {
+    const words = input.split(' ');
+    let lineLength = 0;
+    let result = '';
+
+    for (const word of words) {
+      if (lineLength + word.length > maxLineLength) {
+        result += '\n';
+        lineLength = 0;
+      }
+
+      result += word + ' ';
+      lineLength += word.length + 1;
+    }
+
+    return result;
+  }
+
   private async makeJobResponse(jobs: Job<any, any, string>[]) {
     const result = await Promise.all(
       jobs.map(
         async (job) =>
-          `job: ${job.id}, name: ${job.name}, status: ${await job.getState()}`,
+          `--------------------------------------------------
+job id: ${job.id} (${await job.getState()})
+${
+  job.failedReason
+    ? `\n - reason: ${this.formatText(
+        job.failedReason,
+        50,
+      )}\n - stacktrace: ${this.formatText(job.stacktrace[0], 50)} `
+    : ``
+}
+--------------------------------------------------\n**************************************************\n`,
       ),
     );
 
@@ -40,7 +68,12 @@ export class BullJobCommand extends CommandRunner {
         case 'all':
           const jobs = await this.eventQueue.getJobs();
           Logger.log('Show all bull jobs');
-          await this.makeJobResponse(jobs);
+          await this.makeJobResponse(jobs.sort());
+          break;
+        case 'failed':
+          const failedJobs = await this.eventQueue.getFailed();
+          Logger.log('Show all failed job');
+          await this.makeJobResponse(failedJobs);
           break;
         default:
           console.log('Unknown command');
@@ -54,6 +87,15 @@ export class BullJobCommand extends CommandRunner {
     description: 'Show all jobs',
   })
   parseAll() {
+    return true;
+  }
+
+  @Option({
+    name: 'failed',
+    flags: '-f, --failed',
+    description: 'Show all failed jobs',
+  })
+  parseFailed() {
     return true;
   }
 }
