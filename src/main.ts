e217eb15775824ebdc084b56a44eb856a1c92eed
Jeon-man <jhmjhm0106@naver.com>
2024-01-31 17:07:51 +0900

    REFACTOR: Using `CommandFactoryRunOptions`

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { CommandFactory } from 'nest-commander';
 
 async function bootstrap() {
-  await CommandFactory.run(AppModule, ['warn', 'error', 'debug', 'log']);
+  await CommandFactory.run(AppModule, {
+    logger: ['warn', 'error', 'debug', 'log'],
+  });
 }
 bootstrap();
